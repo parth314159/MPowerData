97c75ad18751845c98ce706688d549a33d23be81
Parth Shah <shahparth8891@gmail.com>
2017-09-24 13:06:12 -0400

    Fix files

--- Authenticate.py
+++ Authenticate.py
@@ -4,8 +4,8 @@ class Auth:
 
     def __init__(self):
         # If you have your config file set up you can run:
-        self.email= 'username'
-        self.password = 'password'
+        self.email= 'shahparth8891@gmail.com'
+        self.password = 'P@rth8891'
         self.syn = synapseclient.login(email=self.email, password=self.password, rememberMe=True)
 
     def getSynObject(self):
