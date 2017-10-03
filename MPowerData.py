95d6f04953fe2b52328d54a577c727c9b335bc19
Parth Shah <shahparth8891@gmail.com>
2017-10-03 19:38:16 -0400

    remove non feature columns

--- MPowerData.py
+++ MPowerData.py
@@ -2,6 +2,9 @@ import datetime
 import json
 import math
 import sys
+import csv
+import numpy as np
+import MPowerDataSet
 
 
 class MPowerDTO:
@@ -24,12 +27,19 @@ class MPowerDTO:
         self.accel_walking_rest_file = accel_walking_rest_file
         self.deviceMotion_walking_rest_file = deviceMotion_walking_rest_file
         self.medTimepoint = medTimepoint
+
         self.minAcceleration = sys.float_info.max
         self.maxAcceleration = 0
         self.diffTimestampAcceleration = 0
-        self.minDeviceMotion = sys.float_info.max
-        self.maxDeviceMotion = 0
-        self.diffTimestampDeviceMotion = 0
+        self.signalEnergyX = 0
+        self.signalEnergyY = 0
+        self.signalEnergyZ = 0
+        self.standardDeviationPedometer = 0
+        # self.minDeviceMotion = sys.float_info.max
+        # self.maxDeviceMotion = 0
+        # self.diffTimestampDeviceMotion = 0
+        self.averageStepDistance = 0
+        self.averageAccelerationResting = 0
 
     def parseDFRow(self, row, file_dictionary_int, file_dictionary_float):
         self.recordId = row['recordId']
@@ -41,13 +51,13 @@ class MPowerDTO:
         self.deviceMotion_walking_outbound_file = file_dictionary_int.get (
             str (row['deviceMotion_walking_outbound.json.items']))
         self.pedometer_walking_outbound_file = file_dictionary_float.get (
-            str (row['pedometer_walking_outbound.json.items']).rstrip ('.0'))
+            "{:.0f}".format (row['pedometer_walking_outbound.json.items']))
         self.accel_walking_return_file = file_dictionary_float.get (
-            str (row['accel_walking_return.json.items']).rstrip ('.0'))
+            "{:.0f}".format (row['accel_walking_return.json.items']))
         self.deviceMotion_walking_return_file = file_dictionary_float.get (
-            str (row['deviceMotion_walking_return.json.items']).rstrip ('.0'))
+            "{:.0f}".format (row['deviceMotion_walking_return.json.items']))
         self.pedometer_walking_return_file = file_dictionary_float.get (
-            str (row['pedometer_walking_return.json.items']).rstrip ('.0'))
+            "{:.0f}".format (row['pedometer_walking_return.json.items']))
         self.accel_walking_rest_file = file_dictionary_int.get (str (row['accel_walking_rest.json.items']))
         self.deviceMotion_walking_rest_file = file_dictionary_int.get (
             str (row['deviceMotion_walking_rest.json.items']))
@@ -55,10 +65,10 @@ class MPowerDTO:
 
     def printObj(self):
         print (self.recordId)
-        print (self.healthcode)
-        print (self.createdOn)
-        print (self.appVersion)
-        print (self.phoneInfo)
+        # print (self.healthcode)
+        # print (self.createdOn)
+        # print (self.appVersion)
+        # print (self.phoneInfo)
         print (self.accel_walking_outbound_file)
         print (self.deviceMotion_walking_outbound_file)
         print (self.pedometer_walking_outbound_file)
@@ -67,12 +77,35 @@ class MPowerDTO:
         print (self.pedometer_walking_return_file)
         print (self.accel_walking_rest_file)
         print (self.deviceMotion_walking_rest_file)
-        print (self.medTimepoint)
+        # print (self.medTimepoint)
         print ()
 
-    def extractMinMaxDiffAcceleration(self):
+    def extractallfeatures(self):
+        #self.printObj ()
+        self.extractMinMaxDiffAccelerationEnergy ()
+        self.extractAvgDistance ()
+        self.extactAverageAcceleraionResting ()
+        print ("Minimum Acceleration - ", self.minAcceleration)
+        print ("Maximum Acceleration - ", self.maxAcceleration)
+        print ("Difference Timestamp - ", self.diffTimestampAcceleration)
+        print ("Average Distance - ", self.averageStepDistance)
+        print ("Resting average acceleration - ", self.averageAccelerationResting)
+        print ("Signal energy X - ", self.signalEnergyX)
+        print ("Signal energy Y - ", self.signalEnergyY)
+        print ("Signal energy Z - ", self.signalEnergyZ)
+        print ("Pedometer Standard Deviation - ", self.standardDeviationPedometer)
+        print (self.phoneInfo)
+        print (self.medTimepoint)
+        print ()
+        self.savefeaturestocsv ()
+
+    def extractMinMaxDiffAccelerationEnergy(self):
         minTimeStamp = 0
         maxTimeStamp = 0
+        x_array = []
+        y_array = []
+        z_array = []
+        count = 0
         with open (self.accel_walking_outbound_file, 'r') as f:
             array = json.load (f)
 
@@ -80,44 +113,88 @@ class MPowerDTO:
         for i in range (len (array)):
             obj = array[i]
             totalAcceleration = math.sqrt (
-                obj.get ('x') * obj.get ('x') + obj.get ('y') * obj.get ('y') + obj.get ('z') * obj.get ('z'))
+                obj.get ('x') ** 2 + obj.get ('y') ** 2 + obj.get ('z') ** 2)
+            self.signalEnergyX += obj.get ('x') ** 2
+            self.signalEnergyY += obj.get ('y') ** 2
+            self.signalEnergyZ += obj.get ('z') ** 2
+            count += 1
             if self.minAcceleration > totalAcceleration:
                 self.minAcceleration = totalAcceleration
                 minTimeStamp = obj.get ('timestamp')
-
             if self.maxAcceleration < totalAcceleration:
                 self.maxAcceleration = totalAcceleration
                 maxTimeStamp = obj.get ('timestamp')
-
+        self.signalEnergyX = self.signalEnergyX / count
+        self.signalEnergyY = self.signalEnergyY / count
+        self.signalEnergyZ = self.signalEnergyZ / count
         self.diffTimestampAcceleration = maxTimeStamp - minTimeStamp
-        print (self.minAcceleration)
-        print (self.maxAcceleration)
-        print (self.diffTimestampAcceleration)
-        print ()
 
-    # def extractMinMaxDiffDeviceMotion(self):
-    #     minTimeStamp = 0
-    #     maxTimeStamp = 0
-    #     with open (self.deviceMotion_walking_outbound_file, 'r') as f:
-    #         array = json.load (f)
-    #
-    #     print(self.deviceMotion_walking_outbound_file)
+    def extractAvgDistance(self):
+        # print(self.pedometer_walking_outbound_file)
+        PMreading = []
+        if (self.pedometer_walking_outbound_file == None):
+            return
+        with open (self.pedometer_walking_outbound_file, 'r') as f:
+            array = json.load (f)
+        avg_distance = 0
+        count = 0
 
+        for obj in array:
+            adis = obj['numberOfSteps'] / obj['distance']
+            avg_distance += adis
+            PMreading.append (adis)
+            count += 1
+            # total_distance+=
+        self.standardDeviationPedometer = np.std (PMreading)
+        self.averageStepDistance = avg_distance / count
+
+    def extactAverageAcceleraionResting(self):
+        with open (self.accel_walking_rest_file, 'r') as f:
+            array = json.load (f)
+        totalAcceleration = 0
+        count = 0
         # print(array[0])
-        # for i in range (len (array)):
-        #     obj = array[i]
-        #     totalAcceleration = math.sqrt (
-        #         obj.get ('x') * obj.get ('x') + obj.get ('y') * obj.get ('y') + obj.get ('z') * obj.get ('z'))
-        #     if self.minDeviceMotion > totalAcceleration:
-        #         self.minDeviceMotion = totalAcceleration
-        #         minTimeStamp = obj.get ('timestamp')
-        #
-        #     if self.maxDeviceMotion < totalAcceleration:
-        #         self.maxDeviceMotion = totalAcceleration
-        #         maxTimeStamp = obj.get ('timestamp')
-        #
-        # self.diffTimestampDeviceMotion = maxTimeStamp - minTimeStamp
-        # print (self.minDeviceMotion)
-        # print (self.maxDeviceMotion)
-        # print (self.diffTimestampDeviceMotion)
-        # print ()
+        for obj in array:
+            Acceleration = math.sqrt (
+                obj.get ('x') * obj.get ('x') + obj.get ('y') * obj.get ('y') + obj.get ('z') * obj.get ('z'))
+            totalAcceleration += Acceleration
+            count += 1
+        self.averageAccelerationResting = totalAcceleration / count
+
+    def savefeaturestocsv(self):
+        with open (MPowerDataSet.MPowerDataSet.table+"_MPower_features.csv", "a") as csv_file:
+            writer = csv.writer (csv_file, delimiter=',', lineterminator='\n')
+            final = []
+            final.extend ((str (self.recordId), str (self.minAcceleration), str (self.maxAcceleration),
+                           str (self.diffTimestampAcceleration), str (self.averageStepDistance),
+                           str (self.standardDeviationPedometer), str (self.averageAccelerationResting),
+                           str (self.signalEnergyX), str (self.signalEnergyY), str (self.signalEnergyZ), str(self.medTimepoint)))
+            #print (final)
+            writer.writerow (final)
+
+            # def extractMinMaxDiffDeviceMotion(self):
+            #     minTimeStamp = 0
+            #     maxTimeStamp = 0
+            #     with open (self.deviceMotion_walking_outbound_file, 'r') as f:
+            #         array = json.load (f)
+            #
+            #     print(self.deviceMotion_walking_outbound_file)
+
+            # print(array[0])
+            # for i in range (len (array)):
+            #     obj = array[i]
+            #     totalAcceleration = math.sqrt (
+            #         obj.get ('x') * obj.get ('x') + obj.get ('y') * obj.get ('y') + obj.get ('z') * obj.get ('z'))
+            #     if self.minDeviceMotion > totalAcceleration:
+            #         self.minDeviceMotion = totalAcceleration
+            #         minTimeStamp = obj.get ('timestamp')
+            #
+            #     if self.maxDeviceMotion < totalAcceleration:
+            #         self.maxDeviceMotion = totalAcceleration
+            #         maxTimeStamp = obj.get ('timestamp')
+            #
+            # self.diffTimestampDeviceMotion = maxTimeStamp - minTimeStamp
+            # print (self.minDeviceMotion)
+            # print (self.maxDeviceMotion)
+            # print (self.diffTimestampDeviceMotion)
+            # print ()
--- MPowerDataSet.py
+++ MPowerDataSet.py
@@ -2,17 +2,23 @@ import synapseclient
 from Authenticate import Auth
 from MPowerData import MPowerDTO
 import warnings
+import csv
 
 class MPowerDataSet:
 
+    table = "syn10146553"
+
     def __init__(self):
         self.syn = Auth().getSynObject()
-        self.table = "syn5713119"
         self.file_dictionary_int = None
         self.file_dictionary_float = None
         self.example_query = None
         self.data_frame = None
         self.dataList = []
+
+        with open(MPowerDataSet.table+"_MPower_features.csv", "w") as outcsv:
+            writer = csv.writer(outcsv, lineterminator='\n')
+            writer.writerow(["recordId", "Feature1", "Feature2", "Feature3", "Feature4", "Feature5", "Feature6", "Feature7", "Feature8" , "Feature9", "medTimePoint"])
 
         self.queyTableAll()
         self.saveAllfilesOffline()
@@ -22,7 +28,7 @@ class MPowerDataSet:
 
     def queyTableAll(self):
         # Query the first 10 rows and all columns of the walking training demographics table
-        self.example_query = self.syn.tableQuery("SELECT * FROM "+self.table)
+        self.example_query = self.syn.tableQuery("SELECT * FROM "+MPowerDataSet.table)
 
     def saveAsListObjects(self):
         print()
@@ -31,7 +37,7 @@ class MPowerDataSet:
             obj.parseDFRow(row,self.file_dictionary_int,self.file_dictionary_float)
             #obj.printObj()
             self.dataList.append(obj)
-            obj.extractMinMaxDiffAcceleration()
+            obj.extractallfeatures()
             # obj.extractMinMaxDiffDeviceMotion()
 
     def saveAsCSV(self):
@@ -40,7 +46,15 @@ class MPowerDataSet:
 
 
     def saveAllfilesOffline(self):
-        warnings.filterwarnings("ignore")
-        self.file_dictionary_int = dict(self.syn.downloadTableColumns(self.example_query, ['accel_walking_outbound.json.items', 'deviceMotion_walking_outbound.json.items', 'accel_walking_rest.json.items', 'deviceMotion_walking_rest.json.items']).items())
-        self.file_dictionary_float = dict(self.syn.downloadTableColumns(self.example_query, [ 'pedometer_walking_outbound.json.items', 'accel_walking_return.json.items', 'deviceMotion_walking_return.json.items', 'pedometer_walking_return.json.items']).items())
-        #print(self.file_dictionary_float)
+        #warnings.filterwarnings("ignore")
+        try:
+            self.file_dictionary_int = dict(self.syn.downloadTableColumns(self.example_query, 'accel_walking_outbound.json.items').items())
+
+            self.file_dictionary_int = dict(self.syn.downloadTableColumns(self.example_query,'accel_walking_rest.json.items').items())
+
+            #self.file_dictionary_float = dict(self.syn.downloadTableColumns(self.example_query, [ 'pedometer_walking_outbound.json.items', 'accel_walking_return.json.items', 'deviceMotion_walking_return.json.items', 'pedometer_walking_return.json.items']).items())
+
+            self.file_dictionary_float = dict(self.syn.downloadTableColumns(self.example_query, 'pedometer_walking_outbound.json.items').items())
+            #print(self.file_dictionary_float)
+        except synapseclient.exceptions.SynapseTimeoutError:
+            self.saveAllfilesOffline()
